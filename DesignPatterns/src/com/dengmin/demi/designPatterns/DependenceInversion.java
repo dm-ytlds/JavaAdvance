ca6394f4c4ae122e4c73a6effdbd9ad61cecbb3e
Administrator <dm-ytlds@outlook.com>
2021-03-30 13:31:06 +0800

    增加设计模式的学习模块

---
+++ DesignPatterns/src/com/dengmin/demi/designPatterns/DependenceInversion.java
@@ -0,0 +1,31 @@
+package com.dengmin.demi.designPatterns;
+
+interface IReceiver {
+    String getInfo();
+}
+
+public class DependenceInversion {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.receive(new Email());
+    }
+}
+
+// 邮件类
+class Email implements IReceiver {
+    public String getInfo() {
+        return "Email!";
+    }
+}
+/*
+ * 方式1：简单，容易实现。但问题是不太容易扩展，比如接收其他消息，就需要对代码进行多出修改
+ * 引入方式2：加入一个接口IReceiver，表示接收者，这样Person类与接口IReceiver发生依赖。从而达到良好的扩展性。
+ * */
+
+class Person {
+    // 接收方法，创建对象，调用方法
+    public void receive(IReceiver receiver) {
+        System.out.println(receiver.getInfo());
+    }
+}
+
